70b41209db8665500e51526450940f41ee7dab93
CS-Kiran <kiran2002bp@gmail.com>
2024-08-10 22:24:58 +0530

    Binary Search Questions Part2

---
+++ BinarySearch/InfiniteArray.java
@@ -0,0 +1,46 @@
+// Find Position of an element is a sorted array of Infinite Numbers
+
+// Since array is infinite, not length function can be used and we don't know  start and end
+// So we search in chunk
+package BinarySearch;
+
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28,
+                29, 30, 31, 32 };
+        int target = 32;
+        if (searchInChunk(nums, target) == -1)
+            System.out.println(target + " Not Found");
+        else
+            System.out.println(target + " Found at index : " + searchInChunk(nums, target));
+    }
+
+    static int searchInChunk(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int temp = end + 1;
+
+            //double the box size ---> previous end + (size of box * 2)
+            end = end + (end - start + 1) * 2; // This formula will prevent the array out of bound error
+            // This is formula to calculate length of str
+            
+            start = temp;
+        }
+        return search(arr, start, end, target);
+    }
+
+    static int search(int[] chunk, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < chunk[mid])
+                end = mid - 1;
+            else if (target > chunk[mid])
+                start = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+}
--- BinarySearch/SearchRange.java
+++ BinarySearch/SearchRange.java
@@ -2,8 +2,50 @@
 
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class SearchRange {
     public static void main(String[] args) {
+        int[] nums = { 5, 7, 7, 8, 8, 8, 10, 11, 12 };
+        int target = 8;
+
+        System.out.println(Arrays.toString(nums));
+        System.out.println(target + " First and Last Position is : " + Arrays.toString(searchRange(nums, target)));
+    }
+
+    static int[] searchRange(int[] arr, int target) {
+
+        int[] result = { -1, -1 };
+        int start = search(arr, target, true); // Finding start index
+        int end = search(arr, target, false); // Finding end index
+
+        result[0] = start;
+        result[1] = end;
+
+        return result;
+    }
+
+    static int search(int[] arr, int target, boolean isFindingStartIndex) {
+        int start = 0;
+        int end = arr.length - 1;
         
+        int ans = -1; // Potential answer
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid])
+            end = mid - 1;
+            else if (target > arr[mid])
+            start = mid + 1;
+            else {
+                ans = mid;
+                // Recursively loop till we get start and end number
+                if (isFindingStartIndex)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return ans;
     }
 }
